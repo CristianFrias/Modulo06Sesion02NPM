59672f9a9ecdd14a558204898e285e782caef5f9
Cristián Andrés Frías García <cristianfrias@MacBook-Air-de-Cristian.local>
2024-10-08 00:26:34 -0300

    Ejemplo con dependencia Faker

--- index.js
+++ index.js
@@ -2,21 +2,12 @@ console.log("Introducción a NPM");
 
 // ESM
 import { validate, clean, format, getCheckDigit } from 'rut.js';
-import { faker } from '@faker-js/faker';
+import { createRandomUser, calcularEdad } from './utils.js';
 
 const valid01 = validate('16.659.851-6')
 const valid02 = validate('11.111.111-0')
 console.log({valid01, valid02});
 
-function createRandomUser() {
-    return {
-        userId: faker.string.uuid(),
-        username: faker.internet.userName(),
-        email: faker.internet.email(),
-        avatar: faker.image.avatar(),
-        password: faker.internet.password(),
-        birthdate: faker.date.birthdate(),
-        registeredAt: faker.date.past(),
-        };
-}
-console.log(createRandomUser());
+const user = createRandomUser();
+user.age = calcularEdad(user.birthdate)
+console.log(user)
